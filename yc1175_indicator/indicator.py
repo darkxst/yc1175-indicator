697c5251636f8e84f77cc6f100e7cc324cf7b031
Tim Lunn <tim@feathertop.org>
2023-11-21 16:27:08 +1100

    clean up type hints

--- yc1175_indicator/indicator.py
+++ yc1175_indicator/indicator.py
@@ -10,14 +10,15 @@ from .const import (
 import logging
 import serial_asyncio
 import struct
+from typing import Tuple
 
 _LOGGER = logging.getLogger(__name__)
 
 class Frame:
     g_seq = 0
-    def __init__(self, seq:bytes(1)=None, msg:bytearray=None):
-        if msg:
-            self.unpack(msg)
+    def __init__(self, seq:bytes(1) = None, packet:bytearray = None) -> None:
+        if packet:
+            self.unpack(packet)
         else:
             self.cmd = ZERO
             self.data = bytearray()
@@ -31,13 +32,13 @@ class Frame:
                 self.seq = self.g_seq
                 self.g_seq = (self.g_seq + 1) % 255
  
-    def build(self, mtype:bytes(1), cmd:bytes(1), data:bytearray=b''):
+    def build(self, mtype:bytes(1), cmd:bytes(1), data:bytearray=b'') -> None:
         self.mtype = mtype
         self.cmd = cmd
         self.data = data
 
-    def led(self, idx:int, effect:int, rgb:tuple):
-        """Build packet to control led, can be called multiple times to add leds to packet."""
+    def led(self, idx:int, effect:int, rgb:tuple) -> None:
+        """Build data field to control led, can be called multiple times to add extra led commands."""
         if not self.validate_rgb(rgb):
             _LOGGER.warning("Invalid rgb tuple, using default value")
             rgb = (255,0,0)
@@ -58,12 +59,12 @@ class Frame:
         frame += CRC16(frame).to_bytes(2)
         return frame
 
-    def unpack(self, packet:bytearray) -> None:
+    def unpack(self, packet:bytes) -> None:
         N = len(packet) - 8
         tuple = struct.unpack(f">cHccc{N}sH", packet)
         sof, plen, self.mtype, self.cmd, self.seq, self.data, crc = tuple
 
-    def unpack_button(self):
+    def unpack_button(self) -> Tuple[bytes, bytes]:
         if self.cmd == COMMAND['REPORT_EVENT']:
             idx, event, param = struct.unpack(">ccH", self.data)
             return idx, event
@@ -83,14 +84,14 @@ class ycProtocol:
             self.reader = reader
             self.writer = writer
 
-    async def serial_init(self):
+    async def serial_init(self) -> None:
         self.reader, self.writer = await serial_asyncio.open_serial_connection(url='/dev/ttyS3', baudrate=115200)
         try:
             asyncio.create_task(self.read_serial(self.reader))
         except asyncio.CancelledError:
             pass
 
-    async def read_serial(self, serial:asyncio.StreamReader):
+    async def read_serial(self, serial:asyncio.StreamReader) -> None:
         while True:
             # Read until the start of frame is found
             header = await serial.readuntil(SOF)
@@ -99,10 +100,10 @@ class ycProtocol:
 
             f_bytes = await serial.readexactly(2)
             f_size = int.from_bytes(f_bytes)
-            frame = SOF + f_bytes + await serial.readexactly(f_size - 3)
+            packet = SOF + f_bytes + await serial.readexactly(f_size - 3)
 
-            if self.checkCRC(frame):
-                asyncio.create_task(self.frame_callback(frame))
+            if self.checkCRC(packet):
+                asyncio.create_task(self.frame_callback(packet))
             else:
                 _LOGGER.warning("CRC Error in frame")
 
@@ -113,8 +114,8 @@ class ycProtocol:
         calc_crc = CRC16(packet[:-crc_size]).to_bytes(crc_size)
         return calc_crc == packet[-crc_size:]
 
-    async def frame_callback(self, packet:bytearray):
-        frame = Frame(msg=packet)
+    async def frame_callback(self, packet:bytearray) -> None:
+        frame = Frame(packet=packet)
         self.log_frame(frame)
 
         if frame.mtype == CMD_TYPE["REQUEST"]:
@@ -135,7 +136,7 @@ class ycProtocol:
             #Notification type not implemented
             pass
 
-    def log_frame(self, frame:Frame, title:str=None):
+    def log_frame(self, frame:Frame, title:str=None) -> None:
         if not title:
             title = "res" if frame.mtype == CMD_TYPE["RESPONSE"] else "req"
         packet = frame.pack()
@@ -154,13 +155,13 @@ class ycProtocol:
 
             self.send_frame(response)
 
-    def send_frame(self, frame):
+    def send_frame(self, frame) -> None:
         self.log_frame(frame)
         if frame.mtype == CMD_TYPE["REQUEST"]:
             self.listeners.append(frame.seq)
         asyncio.create_task(self.write_serial(self.writer, frame))
 
-    async def write_serial(self, serial:asyncio.StreamWriter, frame:Frame):
+    async def write_serial(self, serial:asyncio.StreamWriter, frame:Frame) -> None:
         packet = frame.pack()
         if packet:
             serial.write(packet)
@@ -172,29 +173,29 @@ class HassAPI:
         if yc:
             self.comm = yc
 
-    async def setup(self):
+    async def setup(self) -> None:
         """Setup asyncio serial connection to yc1175"""
         self.comm = ycProtocol()
         await self.comm.serial_init()
 
-    def effect_list(self):
+    def effect_list(self) -> list:
         '''Supported effects list'''
         return EFFECT_LIST
 
-    def light_on(self, idx:int, effect:int=1, rgb:tuple=(0,0,255)):
+    def light_on(self, idx:int, effect:int=1, rgb:tuple=(0,0,255)) -> None:
         led_frame = Frame()
         led_frame.led(idx, effect, rgb)
         self.comm.send_frame(led_frame)
 
-    def light_off(self, idx:int):
+    def light_off(self, idx:int) -> None:
         rgb=(0,0,0)
         led_frame = Frame()
         led_frame.led(idx, 0, rgb)
         self.comm.send_frame(led_frame)
 
-    def register_event_callback(self, callback):
+    def register_event_callback(self, callback) -> None:
         """Register callback to be fired on button press"""
         self.comm.event_callback = callback
 
-    def remove_event_callback(self):
+    def remove_event_callback(self) -> None:
         self.comm.event_callback = None
